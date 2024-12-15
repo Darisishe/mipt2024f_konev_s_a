3b37f6fc3616b2c272253176e91ff6700eb8fed5
Darisishe <konev.sa@phystech.edu>
2024-12-16 02:54:42 +0300

    Almost good version

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import json
 import numpy as np
 import cv2
 
-from sklearn.cluster import KMeans
-
-def ensure_quadrilateral(polygon):
-    hull = cv2.convexHull(polygon)
 
 def blur_kernel_size(bounding_box):
     side_lengths = [
@@ -16,7 +12,8 @@ def blur_kernel_size(bounding_box):
     
     # Compute the average side length
     average_dim = int(np.mean(side_lengths))
-    kernel_size = max(3, (average_dim // 10) | 1)  # At least 3 and odd
+    kernel_size = max(1, int(average_dim * 0.01) | 1)  # At least 1 and odd
+    print("Kernel size: ", kernel_size)
 
     return kernel_size
 
@@ -41,15 +38,15 @@ def dilation_kernel(polygon):
 
     # Define kernel size as a fraction of the average side length
     # Fraction value was choosen empirically (about 8.5%)
-    kernel_size = int(avg_side_length * 0.085)
-    
+    kernel_size = max(1, int(avg_side_length * 0.01))
+    print("Dilation size: ", kernel_size)
     return np.ones((kernel_size, kernel_size), np.uint8)
 
 
 def create_expanded_mask(polygon, img):
     # Create a mask for the barcode area
-    mask = np.zeros_like(img, dtype=np.uint8)
-    cv2.fillConvexPoly(mask, polygon.astype(int), (255, 255, 255))
+    mask = np.zeros(img.shape[:2], dtype=np.uint8)
+    cv2.fillConvexPoly(mask, polygon.astype(int), 255)
     
     # Expand mask a little
     expanded_mask = np.zeros_like(mask)
@@ -60,57 +57,102 @@ def create_expanded_mask(polygon, img):
     return cv2.GaussianBlur(expanded_mask, (kernel_size, kernel_size), 0)
 
 
+def get_background_and_foreground(image, polygon):
+    """
+    Extracts the dominant foreground and background masks from a region.
+    
+    Args:
+        image (ndarray): The source image (BGR format).
+        polygon (ndarray): The polygon defining the barcode area.
+    
+    Returns:
+        tuple: (background_mask, foreground_mask).
+    """
+    init_mask = np.zeros(image.shape[:2], dtype=np.uint8)
+    cv2.fillConvexPoly(init_mask, polygon.astype(int), 255)
+    mask = np.zeros_like(init_mask)
+    cv2.dilate(init_mask, np.ones((3, 3), np.uint8), dst=mask)
+    border_mask = mask & cv2.bitwise_not(init_mask)
+    # Extract the ROI based on the mask
+    roi = cv2.bitwise_and(image, image, mask=mask)
+    
+    # Convert to grayscale within the ROI for thresholding
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    # We have two regions: the foreground and background
+    foreground_mask = (binary == 0) & (mask == 255)
+    background_mask = (binary == 255) & (mask == 255)
+    
+    # The background mask is the one, that has the most intersection with border_mask
+    if cv2.countNonZero(255 * (foreground_mask & border_mask)) > cv2.countNonZero(255 * (background_mask & border_mask)):
+        background_mask, foreground_mask = foreground_mask, background_mask
+    
+    return background_mask, foreground_mask
+    
 def extract_colors(image, polygon):
     """
     Extracts the dominant foreground and background colors from a region.
     
     Args:
         image (ndarray): The source image (BGR format).
-        polygon (ndarray): The quadrilateral defining the barcode area.
+        polygon (ndarray): The polygon defining the barcode area.
     
     Returns:
         tuple: (background_color, foreground_color) as BGR tuples.
     """
-    mask = np.zeros(image.shape[:2], dtype=np.uint8)
-    cv2.fillConvexPoly(mask, polygon.astype(int), 255)
+    background_mask, foreground_mask = get_background_and_foreground(image, polygon)
 
-    # Extract the ROI based on the mask
-    roi = cv2.bitwise_and(image, image, mask=mask)
-    
-    # Convert to grayscale within the ROI for thresholding
-    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-
-    edges = cv2.Canny(gray, 50, 150)
-    
-    # Find connected components
-    _, labels, _, _ = cv2.connectedComponentsWithStats(edges, 8, cv2.CV_32S)
-
-    # We have two regions: the foreground and background. The background has index 0
-    background_label = 0
-    foreground_region = np.zeros_like(roi)
-    foreground_region[labels == background_label] = 255
-    fr = roi[foreground_region > 0]
-    print(fr)
     # Now extract the foreground and background colors
-    foreground_avg_color = np.mean(roi[foreground_region > 0], axis=0)  # Average color of the foreground
-    background_avg_color = np.mean(roi[foreground_region == 0], axis=0)  # Average color of the background
+    foreground_color = np.mean(image[foreground_mask], axis=0)  # Average color of the foreground
+    background_color = np.mean(image[background_mask], axis=0)  # Average color of the background
 
-    return foreground_avg_color, background_avg_color
+    return background_color, foreground_color
 
+def recolor_barcode(image, polygon, background_color, foreground_color):
+    """
+    Recolors barcode on image to a given color scheme.
+    It's better for image to be without noise, shadows, etc.
+    (i.e. image should be synthetic, model etc.)
+    It's needed so found masks would be more accurate
+    
+    Args:
+        image (ndarray): The source image (BGR format).
+        polygon (ndarray): The polygon defining the barcode area.
+        background_color (BGR): New color for background.
+        foreground_color (BGR): New color for bars.
+    
+    Returns:
+        ndarray: New image of barcode with new colors
+    """
 
+    background_mask, foreground_mask = get_background_and_foreground(image, polygon)
+    recolored = image.copy()
+
+    background_color = np.array(background_color, dtype=np.uint8)
+    foreground_color = np.array(foreground_color, dtype=np.uint8)
+    recolored[background_mask] = background_color
+    recolored[foreground_mask] = foreground_color
+
+    return recolored
+
+def correct_colors(base_img, src_polygon, new_barcode, dst_polygon):
+    background_color, foreground_color = extract_colors(base_img, src_polygon)
+    return recolor_barcode(new_barcode, dst_polygon, background_color, foreground_color)
 
 def seamless_replace(base_img, src_polygon, new_barcode, dst_polygon):
+    new_barcode = correct_colors(base_img, src_polygon, new_barcode, dst_polygon)
+
     M = cv2.getPerspectiveTransform(dst_polygon, src_polygon)
     warped_barcode  = cv2.warpPerspective(new_barcode, M, (base_img.shape[1], base_img.shape[0]))
-    
-    mask = create_expanded_mask(src_polygon, base_img)
 
     # Seamless cloning
+    mask = create_expanded_mask(src_polygon, base_img)
+
     center = tuple(np.mean(src_polygon, axis=0).astype(int))
     seamless_result = cv2.seamlessClone(
         warped_barcode, base_img, mask, center, cv2.NORMAL_CLONE
     )
-
     cv2.imshow("Seamless Barcode Replacement", seamless_result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -132,29 +174,102 @@ def show_color(color, window_name="Color"):
 
 def main():
 
-    base_img = cv2.imread("./samples/19.jpg") 
-    new_barcode = cv2.imread("./samples/23.jpg")
-
-    annotations = json.load(open("./annotations.json", "r"))
-    src_region = annotations["_via_img_metadata"]["19.jpg134455"]["regions"][0]
-    dst_region = annotations["_via_img_metadata"]["23.jpg161408"]["regions"][0]
+    ids = [
+		"1.jpg74093",
+		"2.jpg84623",
+		"3.jpg166066",
+		"4.jpg179426",
+		"5.jpg133037",
+		"6.jpg79954",
+		"7.jpg126295",
+		"8.png848842",
+		"9.png621283",
+		"10.jpg146372",
+		"11.jpg129662",
+		"12.jpg129054",
+		"13.jpg73923",
+		"14.jpg141047",
+		"16.jpg103336",
+		"17.jpg64551",
+		"18.jpg54553",
+		"19.jpg134455",
+		"20.jpg119620",
+		"21.jpg103242",
+		"22.jpg56428",
+		"23.jpg161408",
+		"24.jpg81321",
+		"25.jpg49014",
+		"26.jpg81972",
+		"27.jpg171610",
+		"28.jpg199540",
+		"29.jpg79065",
+		"30.jpg53070",
+		"31.jpg97889",
+		"32.jpg75349"]
     
-    src_all_points_x = src_region["shape_attributes"]["all_points_x"]
-    src_all_points_y = src_region["shape_attributes"]["all_points_y"]
+    names = [
+		"1.jpg",
+		"2.jpg",
+		"3.jpg",
+		"4.jpg",
+		"5.jpg",
+		"6.jpg",
+		"7.jpg",
+		"8.png",
+		"9.png",
+		"10.jpg",
+		"11.jpg",
+		"12.jpg",
+		"13.jpg",
+		"14.jpg",
+		"16.jpg",
+		"17.jpg",
+		"18.jpg",
+		"19.jpg",
+		"20.jpg",
+		"21.jpg",
+		"22.jpg",
+		"23.jpg",
+		"24.jpg",
+		"25.jpg",
+		"26.jpg",
+		"27.jpg",
+		"28.jpg",
+		"29.jpg",
+		"30.jpg",
+		"31.jpg",
+		"32.jpg"]
+    for name, id in zip(names, ids):
+            
+        base_img = cv2.imread("./samples/" + name) 
+        new_barcode = cv2.imread("./image.png")
 
-    dst_all_points_x = dst_region["shape_attributes"]["all_points_x"]
-    dst_all_points_y = dst_region["shape_attributes"]["all_points_y"]
+        annotations = json.load(open("./annotations.json", "r"))
+        src_region = annotations["_via_img_metadata"][id]["regions"][0]
+        
+        src_all_points_x = src_region["shape_attributes"]["all_points_x"]
+        src_all_points_y = src_region["shape_attributes"]["all_points_y"]
 
-    src_polygon = np.float32(list(zip(src_all_points_x, src_all_points_y)))
-    print(src_polygon)
+        dst_all_points_x = [145,266,264,145]
+        dst_all_points_y = [165,165,44,45]
 
-    dst_polygon = np.float32(list(zip(dst_all_points_x, dst_all_points_y)))
-    print(dst_polygon)
 
-    foreground_color, background_color = extract_colors(base_img, src_polygon)
-    show_color(foreground_color, "Foreground")
-    show_color(background_color, "Background")
-    # seamless_replace(base_img, src_polygon, new_barcode, dst_polygon)
+        src_polygon = np.float32(list(zip(src_all_points_x, src_all_points_y)))
+        
+        if src_polygon.shape[0] > 4:
+            continue
+
+        print(name)
+
+        dst_polygon = np.float32(list(zip(dst_all_points_x, dst_all_points_y)))
+
+        # foreground_color, background_color = extract_colors(base_img, src_polygon)
+        # show_color(foreground_color, "Foreground")
+        # show_color(background_color, "Background")
+        # new_barcode = correct_colors(base_img, src_polygon, new_barcode, dst_polygon)
+        # cv2.imshow("New barcode recolored", new_barcode)
+        # cv2.waitKey()
+        seamless_replace(base_img, src_polygon, new_barcode, dst_polygon)
 
    
 
